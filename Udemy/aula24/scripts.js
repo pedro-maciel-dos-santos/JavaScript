c10842e021c5b93898852a0d6608c7a57386b0e0
Pedro Maciel dos Santos <pe.maciel.santos@gmail.com>
2025-04-21 11:27:10 -0300

    aula Udemy JavaScript

--- Udemy/aula24/scripts.js
+++ Udemy/aula24/scripts.js
@@ -7,7 +7,7 @@
 
 Value refere-se ao valor dentro do gerador
 
-done mrefere-se se o gerador acabou ou não, no caso é FALSE, ainda não acabou pois possui o Valor 2 e Valor 3.
+done refere-se se o gerador acabou ou não, no caso é FALSE, ainda não acabou pois possui o Valor 2 e Valor 3.
 
 para chamar o yield precisa utilizar o .next() e para conseguir pegar o valor separado utiliza .value no final no next:
 
@@ -29,6 +29,8 @@ function* geradora1() {
     yield 'Valor 3';
 }
 const g1 = geradora1();
+console.log(g1.next());
+console.log(g1.next());
 /*
 const g1 = geradora1();
 for (let valor of g1) {
@@ -48,7 +50,7 @@ function* geradora2() {
 const g2 = geradora2();
 console.log(g2.next().value);
 */
-
+/*
 function* geradora3() {
     yield 0;
     yield 1;
@@ -62,22 +64,22 @@ function* geradora4() {
     yield 5;
 }
 
-const g4 = geradora4();
+const g4 = geradora4();*/
 /*
 for (let valor of g4) {
     console.log(valor);
 }
 */
-
+/*
 function* geradora5() {
     yield function() {
         console.log('Vim do y1');
     };
-
-    return function() {
+*/
+  /*  return function() {
         console.log('Vim do return');
-    }
-
+    }*/
+/*
     yield function() {
         console.log('Vim do y2');
     };
@@ -88,4 +90,4 @@ const func1 = g5.next().value;
 const func2 = g5.next().value;
 
 func1();
-func2();
+func2();*/
